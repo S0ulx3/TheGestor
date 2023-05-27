e9f1f4c0b60220e330423aba42668b57809b795b
S0ulx3 <98002038+S0ulx3@users.noreply.github.com>
2023-05-27 17:25:09 +0200

    Update Thegestor.sh
    He añadido y arreglado los errores con los colores
    
    I added and fixed the errors with the colours

--- Thegestor.sh
+++ Thegestor.sh
@@ -1,72 +1,75 @@
 #!/bin/bash
 
-# Colores
-CYAN='\033[0;36m'
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # Restablecer el color a predeterminado
-
+# Definir colores
+color_reset="\e[0m"
+color_yellow="\e[33m"
+color_cyan="\e[36m"
+color_green="\e[32m"
+color_red="\e[31m"
 
 while true
 do
-   echo "${YELLOW}===========================================${NC}"
-   echo "${RED}Bienvenido al Gestor de tareas${NC}                "
-   echo "${RED}1)${NC} Mostrar contenido del directorio actual    "
-   echo "${RED}2)${NC} Crear un archivo"
-   echo "${RED}3)${NC} Crear un directorio"
-   echo "${RED}4)${NC} Mover o renombrar archivo/directorio"
-   echo "${RED}5)${NC} Copiar archivo/directorio"
-   echo "${RED}6)${NC} Eliminar archivo/directorio"
-   echo "${RED}7)${NC} Salir"
-   echo "${YELLOW}===========================================${NC}"
+    echo -e "${color_red}Bienvenido al gestor de archivos${color_reset}"
+    echo -e "${color_cyan}1. Mostrar contenido del directorio actual"
+    echo -e "2. Crear un archivo"
+    echo -e "3. Crear un directorio"
+    echo -e "4. Mover o renombrar archivo/directorio"
+    echo -e "5. Copiar archivo/directorio"
+    echo -e "6. Eliminar archivo/directorio"
+    echo -e "7. Salir${color_reset}"
 
-   read -p "Ingrese la opcion deseada: " opcion
+    read -p "Ingrese la opción deseada: " opcion
 
-   case $opcion in
-       1) echo
-          ls
-          ;;
-       2)
-          read -p "${CYAN}Ingrese el nombre del archivo:${NC} " nombre_archivo
-          touch $nombre_archivo
-          echo
-          echo "${YELLOW}Archivo${NC} ${RED}$nombre_archivo${NC}${YELLOW} creado${YELLOW}"
-          ;;
+    case $opcion in
+        1) echo
+           ls ;;
+        2)
+            echo
+            read -p "Ingrese el nombre del archivo: " nombre_archivo
+            touch $nombre_archivo
+            2>/dev/null
+            echo
+            echo -e "${color_cyan}Archivo creado:${color_reset} ${color_red}$nombre_archivo${color_reset}"
+            ;;
+        3)
+            echo
+            read -p "Ingrese el nombre del directorio: " nombre_directorio
+            mkdir $nombre_directorio
+            2>/dev/null
+            echo
+            echo -e "${color_cyan}Directorio creado:${color_reset} ${color_red}$nombre_directorio${color_reset}"
+            ;;
+        4)
+            echo
+            read -p "Ingrese el nombre del archivo/directorio: " origen
+            echo
+            read -p "Ingrese la nueva ubicación/nombre: " destino
+            mv $origen $destino
+            2>/dev/null
+            echo
+            echo -e "${color_cyan}Archivo/Directorio${color_reset} ${color_red}$origen${color_reset} ${color_cyan}movido/renombrado a${reset_color} ${color_red}$destino${color_reset}"
+            ;;
+        5)
+            echo
+            read -p "Ingrese el nombre del archivo/directorio: " origen
+            echo
+            read -p "Ingrese la ubicación de destino: " destino
+            cp -r $origen $destino
+            2>/dev/null
+            echo
+            echo -e "${color_cyan}Archivo/Directorio${color_reset} ${color_red}$origen${color_reset} ${color_cyan}copiado a${color_reset} ${color_red}$destino.${color_reset}"
+            ;;
+        6)
+            echo
+            read -p "Ingrese el nombre del archivo/directorio: " nombre_archivo
+            echo
+            rm -rf $nombre_archivo
+            2>/dev/null
+            echo -e "${color_cyan}Archivo/Directorio${color_reset} ${color_red}$nombre_archivo${color_reset} ${color_cyan}eliminado.${color_reset}"
+            ;;
+        7) exit ;;
+        *) echo -e "${color_red}Opción inválida${color_reset}";;
+    esac
 
-       3)
-          read -p "${CYAN}Ingrese el nombre del directorio:${NC} " nombre_directorio
-          mkdir $nombre_directorio
-          echo
-          echo "${YELLOW}Directorio${NC} ${RED}$nombre_directorio${NC} ${YELLOW}creado${NC}"
-          ;;
-
-       4)
-         read -p "${CYAN}Ingrese nombre del archivo:${NC} " origen
-         read -p "${CYAN}Ingrese la nueva ubicacion o nombre:${NC} " destino
-         mv $origen $destino
-         echo
-         echo "${YELLOW}Archivo/directorio${NC} ${RED}$origen${NC} ${YELLOW}movido/renombrado o/a${NC} ${RED}$destino${NC}"
-         ;;
-
-       5)
-          read -p "${CYAN}Ingrese nombre de archivo/directorio:${NC} " origen
-          read -p "${CYAN}Ingrese la ruta donde desea copiar el archivo:${NC} " destino
-          cp $origen $destino
-          echo
-          echo "${YELLOW}Archivo${NC} ${RED}$origen${NC} ${YELLOW}copiado a${NC} ${RED}$destino${NC}"
-          ;;
-
-       6)
-          read -p "${CYAN}Ingrese el nombre del archivo:${NC} " $nombre_archivo
-          rm -rf $nombre_archivo
-          echo
-          echo "${RED}$nombre_archivo${NC} ${YELLOW}ha sido eliminado${NC}"
-          ;;
-       7) exit ;;
-       *) echo "${RED}Opcion invalida${NC}";;
- esac
-
-echo
-
+    echo
 done
